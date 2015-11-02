55acabca4c412b78179d0d4e9e7772de7df17fd3
VictorNS <VictorNS@narod.ru>
2015-11-02 18:34:30 +0600

    Lesson 5 exercise 0 Nesting

---
+++ Lesson5/0_Nesting/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Nesting
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			solution("");
+			solution("(");
+			solution(")");
+			solution("()");
+			solution(")(");
+			solution("(()(())())");
+			solution(new StringBuilder().Insert(0, "(()(())())", 100000).ToString());
+		}
+
+		public static int solution(string S)
+		{
+			if (S.Length>100)
+				Console.WriteLine("Input: " + S.Length);
+			else
+				Console.WriteLine("Input: " + S);
+			if (string.IsNullOrEmpty(S))
+			{
+				Console.WriteLine("Result: 1");
+				return 1;
+			}
+			Stack<bool> stack=new Stack<bool>();
+			foreach (char c in S)
+			{
+				if (c=='(')
+					stack.Push(true);
+				else
+				{
+					if (stack.Count == 0)
+					{
+						Console.WriteLine("Result: 0");
+						return 0;
+					}
+					stack.Pop();
+				}
+			}
+			var res = stack.Count == 0 ? 1 : 0;
+			Console.WriteLine("Result: " + res);
+			return res;
+		}
+	}
+}
