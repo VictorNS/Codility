675cd215e42ba43cad0ab8d95894ce60bc078c66
VictorNS <VictorNS@narod.ru>
2015-11-23 21:23:54 +0600

    Lesson 8 exercise 1 CountFactors

---
+++ Lesson8/1_CountFactors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1_CountFactors
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine(solution(24));
+			Console.WriteLine(solution(36));
+		}
+
+		public static int solution(int N)
+		{
+			Console.WriteLine(N);
+			var res = 0;
+			var number = 1;
+			while (number * number < N)
+			{
+				if (N % number == 0)
+				{
+					res = res + 2;
+					Console.Write("{0} ", number);
+				}
+				number++;
+			}
+			if (number * number == N)
+			{
+				res++;
+				Console.Write("  {0}", number);
+			}
+			Console.WriteLine();
+			return res;
+		}
+	}
+}
