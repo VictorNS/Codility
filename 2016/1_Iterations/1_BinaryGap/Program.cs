e1981fde9844978434c9e4a6ef18036245007798
VictorNS <VictorNS@narod.ru>
2016-01-02 14:17:07 +0600

    1_Iterations

---
+++ 2016/1_Iterations/1_BinaryGap/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _1_BinaryGap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Run(1041);
+			Run(6);
+		}
+		static void Run(int input)
+		{
+			Console.WriteLine();
+			Console.WriteLine("Input: {0}", input);
+			var s = new Solution();
+			var result = s.solution(input);
+			Console.WriteLine("Rssult: {0}", result);
+		}
+	}
+
+	class Solution
+	{
+		public int solution(int N)
+		{
+			var b = Convert.ToString(N, 2);
+			var maxLength = 0;
+			var currentLength = 0;
+			for (int i = 0; i < b.Length; i++)
+			{
+				Console.Write(b[i]);
+				if (b[i] == '1')
+				{
+					maxLength = Math.Max(maxLength, currentLength);
+					currentLength = 0;
+				}
+				else
+				{
+					currentLength++;
+				}
+			}
+			Console.WriteLine();
+			return maxLength;
+		}
+	}
+}
