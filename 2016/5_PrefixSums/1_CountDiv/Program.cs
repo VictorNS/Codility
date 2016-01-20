1171fa9aa6d591cbe7d0ee08a2c184568c4a3345
VictorNS <VictorNS@narod.ru>
2016-01-20 21:01:13 +0600

    5_PrefixSums

---
+++ 2016/5_PrefixSums/1_CountDiv/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _1_CountDiv
+{
+	class Program
+	{
+		static void Main()
+		{
+			Run(0, 0, 11);
+			Run(0, 1, 11);
+			Run(1, 1, 11);
+			Run(1, int.MaxValue, 1);
+			Run(1, int.MaxValue, int.MaxValue);
+
+			Run(0, 0, 1);
+			Run(0, 0, 2000000000);
+			Run(0, 1, 1);
+			Run(0, 1, 2000000000);
+			Run(0, 2000000000, 1);
+			Run(0, 2000000000, 2000000000);
+			Run(1, 2000000000, 1);
+			Run(1, 2000000000, 2000000000);
+			Run(2000000000, 2000000000, 1);
+			Run(2000000000, 2000000000, 2000000000);
+			Run(2000000000, 2000000000, 2000000000 - 1);
+			Run(2000000000 - 1, 2000000000, 2000000000 - 1);
+			Run(2000000000 - 2, 2000000000, 2000000000 - 1);
+			Run(6, 11, 2);
+		}
+		static void Run(int A, int B, int K)
+		{
+			Console.WriteLine("A={0} B={1} K={2}", A, B, K);
+			var s = new Solution();
+			var result = s.solution(A, B, K);
+			Console.WriteLine("Result: {0}", result);
+		}
+	}
+
+	class Solution
+	{
+		public int solution(int A, int B, int K)
+		{
+			//if (K > B)
+			//	return 0;
+			var mod = A % K;
+			Int64 start = (mod == 0) ? (Int64)A : (Int64)A - (Int64)mod + (Int64)K;
+			if (start > B)
+				return 0;
+			return (B - (int)start) / K + 1;
+		}
+	}
+}
---
+++ 2016/5_PrefixSums/2_PassingCars/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _2_PassingCars
+{
+	class Program
+	{
+		static void Main()
+		{
+			var s = new Solution();
+			Console.WriteLine(s.solution(new int[] { 0, 1, 0, 1, 1 }));
+
+			var a1 = new int[10000100];
+			for (int i = 100; i < a1.Length; i++)
+				a1[i] = 1;
+			Console.WriteLine("{0} in array with length: {1}", s.solution(a1), a1.Length);
+
+			var a2 = new int[10000101];
+			for (int i = 100; i < a2.Length; i++)
+				a2[i] = 1;
+			Console.WriteLine("{0} in array with length: {1}", s.solution(a2), a2.Length);
+
+			var a3 = new int[10000000];
+			Console.WriteLine("{0} in array with length: {1}", s.solution(a3), a3.Length);
+
+			var a4 = new int[10000000];
+			for (int i = 0; i < a4.Length; i++)
+				a4[i] = 1;
+			Console.WriteLine("{0} in array with length: {1}", s.solution(a4), a4.Length);
+		}
+
+		class Solution
+		{
+			public int solution(int[] A)
+			{
+				var count = 0;
+				var factor = 0;
+				for (int i = 0; i < A.Length; i++)
+				{
+					if (A[i] == 0)
+						factor++;
+					else
+						count += factor;
+					if (count > 1000000000)
+						return -1;
+				}
+				return count;
+			}
+		}
+	}
+}
